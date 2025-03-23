0f260a65a23d5dcfefbb3c93b351486889b93445
tejusrevi <tejusrevi@gmail.com>
2025-03-22 22:33:21 -0230

    ENH: Add trade executor functions

--- data_infra/tradingOps/main.py
+++ data_infra/tradingOps/main.py
@@ -1,4 +1,21 @@
 # BUY/SELL functionality sits here.
 
+from data_infra.database.tradeAPI import tradeAPI
+
 class tradeExecutor():
-    pass
+    def __init__(self):
+        self.tradeAPI = tradeAPI()
+
+    def executor(self, portfolio_id, ticker, signal, quantity):
+        latest_price = self.tradeAPI.get_latest_price(ticker)
+
+        if signal == "BUY":
+            cash_on_hand = self.tradeAPI.get_portfolio_cash(portfolio_id)
+            if cash_on_hand >= latest_price * quantity:
+                self.tradeAPI.commit_trade(portfolio_id, ticker, signal, quantity, latest_price):
+
+        elif signal == "SELL":
+            current_quantity =  db.get_holding_quantity(portfolio_id, ticker)
+            if current_quantity >= quantity:
+                self.tradeAPI.commit_trade(portfolio_id, ticker, signal, quantity, latest_price):
+
