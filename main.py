5844d11979834312c27dfb70b46ec7e440e2ecca
Joshua Kattapuram <70146035+joshuakatt@users.noreply.github.com>
2025-06-11 23:57:16 -0230

    updated realtimeDataIngestor.py

--- main.py
+++ main.py
@@ -5,12 +5,8 @@ from portfolios.portfolio_2.strategy import SimpleMeanReversion
 def main():
     db = MQSDBConnector()
     portfolio_2 = SimpleMeanReversion(db_connector=db, executor=None)
-    weights_2 = {
-        "AAPL": 0.4,
-        "TSLA": 0.3,
-        "NVDA": 0.3
-    }
-    portfolio_2.backtest(weights_2)
+
+    portfolio_2.backtest()
 
 
     print("Backtest executed successfully.")
