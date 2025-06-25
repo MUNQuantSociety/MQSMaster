1bab627b07ae34b63e8edc85e71652c1169e4846
Joshua Kattapuram <70146035+joshuakatt@users.noreply.github.com>
2025-06-25 15:42:17 -0230

    added comments and setup rules to main and main_backtest.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 # main.py
+# Use main_backtest.py for backtesting purposes.
+# main.py is reserved for live trading purposes.
 
 import logging
 from data_infra.database.MQSDBConnector import MQSDBConnector
@@ -6,11 +8,11 @@ from data_infra.tradingOps.realtime.live import tradeExecutor # For live trading
 from backtest.runner import BacktestRunner  # This is the backtest runner
 
 # Import portfolio classes, not instances
-from portfolios.portfolio_1.strategy import SAMPLE_PORTFOLIO
+from portfolios.portfolio_1.strategy import SimpleMomentum
 from portfolios.portfolio_2.strategy import SimpleMeanReversion
 
 from engines.run_engine import RunEngine
-from engines.backtest_engine import BacktestEngine  # Assuming this is the backtest engine
+from engines.backtest_engine import BacktestEngine  # This is the backtest engine
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
@@ -22,11 +24,10 @@ def main():
     dbconn = MQSDBConnector()
     live_trade_executor = tradeExecutor(dbconn)
 
-    
     run_engine = RunEngine(db_connector=dbconn, executor=live_trade_executor, debug=False)
     # Pass a list of the portfolio classes you want to run
     run_engine.load_portfolios([
-        SAMPLE_PORTFOLIO,
+        SimpleMomentum,
         SimpleMeanReversion
     ])
 
@@ -34,19 +35,5 @@ def main():
     # Start all loaded portfolios. This will block until Ctrl+C is pressed.
     run_engine.run()
     
-    """
-    backtest_engine= BacktestEngine(db_connector=dbconn,backtest_executor=None)  # Replace None with actual backtest executor if available
-
-    backtest_engine.setup(
-        portfolio_classes=[SAMPLE_PORTFOLIO,SimpleMeanReversion],
-        start_date="2023-01-01",
-        end_date="2023-12-31",
-        initial_capital=1000000.0
-    )
-    # Run the backtests
-    backtest_engine.run()
-    
-    # If you want to run backtests, uncomment the above lines and set up the backtest engine accordingly.
-    """
 if __name__ == '__main__':
     main()
