2c383820ec55f628d1822d5cf2688ddb02ba8913
Ayesha973 <ayshaziaudin@gmail.com>
2025-11-20 21:28:42 -0330

    added backtest test, updated API endpoints test, added yml file for CI/CD on main

--- tests/test_api_endpoints.py
+++ tests/test_api_endpoints.py
@@ -1,45 +1,35 @@
-#tests/test_api_endpoints.py
-
+# tests/test_api_endpoints.py
+import pytest
 from datetime import datetime, timedelta
 from src.orchestrator.marketData.fmpMarketData import FMPMarketData
 
-def fmp():
-    return FMPMarketData
+@pytest.fixture
+def start_date():
+    start_date = (datetime.now() - timedelta(days=7))
+    return start_date.strftime("%Y-%m-%d")
 
-def test_dates():
-    start_date = (datetime.now() - timedelta(days=7)).strftime("%Y-%m-%d")
-    end_date = datetime.now().strftime("%Y-%m-%d")
-    return start_date, end_date
+@pytest.fixture
+def end_date():
+    return datetime.now().strftime("%Y-%m-%d")
+
+@pytest.fixture
+def fmp():
+    return FMPMarketData()
+
 
 def test_historical_data(fmp, start_date, end_date):
-    url = f"https://financialmodelingprep.com/api/v3/historical-price-full/AAPL?from={start_date}&to={end_date}&apikey={fmp.fmp_api_key}"
-    response = requests.get(url)
-    assert response.status_code == 200, f"API returned {response.status_code}: {response.text}"
-
     data = fmp.get_historical_data("AAPL", start_date, end_date)
     assert data is not None and isinstance(data, list) and len(data) > 0, "Historical data api failed"
 
 def test_intraday_data(fmp, start_date, end_date):
-    url = f"https://financialmodelingprep.com/api/v3/historical-chart/{fmp.interval_str}/AAPL?from={start_date}&to={end_date}&apikey={fmp.fmp_api_key}"
-    response = requests.get(url)
-    assert response.status_code == 200, f"API returned {response.status_code}: {response.text}"
-    data = fmp.get_intraday_data("AAPL", start_date, end_date)
+    data = fmp.get_intraday_data("AAPL", start_date, end_date, 5)
     assert data is not None and isinstance(data, list) and len(data) > 0, "Intraday data api failed"
 
 def test_realtime_data(fmp, start_date, end_date):
-    url = f"https://financialmodelingprep.com/api/v3/historical-price-full/AAPL?from={start_date}&to={end_date}&apikey={fmp.fmp_api_key}"
-    response = requests.get(url)
-    assert response.status_code == 200, f"API returned {response.status_code}: {response.text}"
-    
-    data = fmp.get_realtime_data("AAPL", start_date, end_date)
+    data = fmp.get_realtime_data("NASDAQ")
     assert data is not None and isinstance(data, list) and len(data) > 0, "Realtime data api failed"
 
-def test_curent_price(fmp, start_date, end_date):
-    url = f"https://financialmodelingprep.com/stable/quote?symbol=AAPL"
-    params = {"apikey": self.fmp_api_key}
-    response = requests.get(url, params)
-    assert response.status_code == 200, f"API returned {response.status_code}: {response.text}"
+def test_curent_price(fmp):
+    price = fmp.get_current_price("AAPL")
+    assert isinstance(price, float) and price > 0, "Current Price api failed"
 
-    price = fmp.get_current_price("AAPl")
-    assert isinstance(data, list) and len(data) > 0, "Current Price api failed"
- 
